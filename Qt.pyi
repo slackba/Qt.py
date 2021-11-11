ad4e5e90a02726ff4d3d2c1bd6dfbda2c921d6b4
slackba <benjamin.slack@thethirdfloorinc.com>
2021-11-11 12:35:14 -0800

    update for wing 8.1

--- Qt.pyi
+++ Qt.pyi
@@ -9,5 +9,18 @@ generated using a WING utility.
 Questions to iam@nimajneb.com if you're
 using wing and need to get this working.
 """
-from WING import *
-print 'imported'
+from WING import QtCore
+from WING import QtGui
+from WING import QtHelp
+from WING import QtMultimedia
+from WING import QtNetwork
+from WING import QtOpenGL
+from WING import QtPrintSupport
+from WING import QtSql
+from WING import QtSvg
+from WING import QtTest
+from WING import QtWidgets
+from WING import QtXml
+from WING import QtXmlPatterns
+
+print('imported Qt stubs')
